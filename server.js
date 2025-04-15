d7b84fc9552a924da5cf73220318f700dcb091b5
sampyle03 <sampyle@outlook.com>
2025-04-15 19:24:11 +0100

    progress1924

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const session = require('express-session');
-const csp = require('express-csp-header');
 const path = require('path');
 const fs = require('fs');
 const { stringify } = require('querystring');
